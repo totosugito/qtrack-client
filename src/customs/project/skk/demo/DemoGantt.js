18a351a12582068eb8ce69dd3d4ee2d2982c8380
toto <toto_sugito_plg@yahoo.co.id>
2024-01-16 16:25:59 +0700

    add gantt export data

--- src/customs/project/skk/demo/DemoGantt.js
+++ src/customs/project/skk/demo/DemoGantt.js
@@ -7,7 +7,7 @@ import {
     Inject,
     Resize,
     Sort,
-    Toolbar, PdfExport,
+    Toolbar, PdfExport, ExcelExport,
 } from "@syncfusion/ej2-react-gantt";
 
 import '../../../../../node_modules/@syncfusion/ej2-base/styles/material.css';
@@ -29,6 +29,7 @@ import {BrLabel, BrProjectList} from "../component";
 import dummy_data from "../../skk/data/demo_project.json"
 import BrLink from "../component/breadcrumbs/br-link";
 import {getRouterUrl} from "../../../router";
+import {read, utils} from "xlsx";
 
 registerLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWGhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEZiWH1ZcHdQRWJZWE12Xg==");
 const DemoGantt = ({ganttId}) => {
@@ -37,7 +38,7 @@ const DemoGantt = ({ganttId}) => {
         container: {
             p: 1,
             marginTop: "-10px",
-            // width: "100vh"
+            // width: "1000px"
         },
         label: {
             mt: 2,
@@ -60,9 +61,93 @@ const DemoGantt = ({ganttId}) => {
     }
     const toolbarClick = (args) => {
         if (args.item.id === 'pdf_export') {
-            ganttChart.pdfExport();
+            let exportProperties= {
+                includeHiddenColumn: false,
+                pageOrientation: 'Landscape',
+                pageSize: 'A1',
+                fitToWidthSettings: {
+                    isFitToWidth: false,
+                }
+            };
+            ganttChart.pdfExport(exportProperties);
         }
-    };
+        else if (args.item.id === 'excel_export') {
+            ganttChart.excelExport();
+        }
+    }
+
+    const reformatXlsx = (inputData) => {
+        const transformedData = {
+            data: []
+        };
+
+        let currentParent = null;
+        let currentChild = null;
+
+        inputData.forEach(item => {
+            const titleTrimmed = item.TaskName.trim();
+
+            if (titleTrimmed.startsWith('Parent')) {
+                // New Parent
+                if (currentChild !== null) {
+                    currentParent.subtasks.push(currentChild);
+                    currentChild = null;
+                }
+
+                if (currentParent !== null) {
+                    transformedData.data.push(currentParent);
+                }
+
+                currentParent = {
+                    TaskId: item.TaskId,
+                    TaskName: titleTrimmed,
+                    subtasks: []
+                };
+            } else {
+                // Child
+                if (currentChild !== null) {
+                    currentParent.subtasks.push(currentChild);
+                }
+
+                currentChild = {
+                    TaskId: item.TaskId,
+                    TaskName: titleTrimmed,
+                    subtasks: []
+                };
+            }
+        });
+
+        // Add the last parent and child
+        if (currentParent !== null) {
+            if (currentChild !== null) {
+                currentParent.subtasks.push(currentChild);
+            }
+            transformedData.data.push(currentParent);
+        } else if (currentChild !== null) {
+            transformedData.data.push(currentChild);
+        }
+
+        return(transformedData["data"]);
+    }
+
+    const handleImport = ($event) => {
+        const files = $event.target.files;
+        if (files.length) {
+            const file = files[0];
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const wb = read(event.target.result);
+                const sheets = wb.SheetNames;
+
+                if (sheets.length) {
+                    const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+                    setGantt(reformatXlsx(rows));
+                }
+            }
+            reader.readAsArrayBuffer(file);
+        }
+    }
+
     return (
         <>
             <Box maxWidth="xl" sx={styles.container}>
@@ -74,11 +159,19 @@ const DemoGantt = ({ganttId}) => {
 
                 <Box sx={{mb: 1}}>
                     <Button variant={'outlined'} onClick={()=> createDummyData()}>Dummy Data</Button>
+
+                    {/*<Box sx={{p: 1}}>*/}
+                    {/*    <input type="file" name="file" className="custom-file-input" id="inputGroupFile"*/}
+                    {/*           required onChange={handleImport}*/}
+                    {/*           accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"/>*/}
+                    {/*</Box>*/}
                 </Box>
                 {gantt !== undefined &&
                     <GanttComponent style={styles.gantt} ref={ganttRef => ganttChart = ganttRef} dataSource={gantt} treeColumnIndex={1}
                                     allowResizing={true} allowSelection={true}
                                     toolbarClick={toolbarClick.bind(this)}
+                                    allowPdfExport={true}
+                                    allowExcelExport={true}
                                     toolbar={[
                                         // {
                                         //     text: 'Save',
@@ -86,13 +179,20 @@ const DemoGantt = ({ganttId}) => {
                                         //     id: 'save_data',
                                         //     prefixIcon: 'e-save'
                                         // },
-                                        // {
-                                        //     text: 'PDF Export',
-                                        //     tooltipText: 'PDF Export',
-                                        //     id: 'pdf_export',
-                                        //     prefixIcon: 'e-save'
-                                        // },
-                                        'Add', 'Edit', 'Update', 'Delete', 'Cancel', 'ExpandAll', 'CollapseAll', 'Indent', 'Outdent']}
+                                        'Add', 'Edit', 'Update', 'Delete', 'Cancel', 'ExpandAll', 'CollapseAll', 'Indent', 'Outdent',
+                                        {
+                                            text: 'PDF Export',
+                                            tooltipText: 'PDF Export',
+                                            id: 'pdf_export',
+                                            prefixIcon: 'e-export-pdf'
+                                        },
+                                        {
+                                            text: 'Excel Export',
+                                            tooltipText: 'Excel Export',
+                                            id: 'excel_export',
+                                            prefixIcon: 'e-export-excel'
+                                        },
+                                    ]}
                                     taskFields={{
                                         id: 'TaskID',
                                         name: 'TaskName',
@@ -110,7 +210,7 @@ const DemoGantt = ({ganttId}) => {
                                         allowTaskbarEditing: true,
                                         showDeleteConfirmDialog: true
                                     }}>
-                        <Inject services={[Edit, Selection, Toolbar, Filter, Sort, Resize, PdfExport]}/>
+                        <Inject services={[Edit, Selection, Toolbar, Filter, Sort, Resize, PdfExport, ExcelExport]}/>
                     </GanttComponent>
                 }
             </Box>
